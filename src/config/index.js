f41cbd44ed97b77c591f3a855d8990c38b892742
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2022-02-13 22:56:05 -0300

    Update index,js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 export const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
-  : 'http://devflix1.herokuapp.com';
+  ? 'https://localhost:8080'
+  : 'https://devflix1.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
